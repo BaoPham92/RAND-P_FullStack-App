32f21ea50f47cb1ad1d5bf793b9beb08e65f0ac2
Cuza <baopham92@yahoo.com>
2019-02-14 13:11:01 -0500

    Server config for GraphQL Playground for mutations & queries

--- server/src/generated/prisma-client/index.js
+++ server/src/generated/prisma-client/index.js
@@ -20,21 +20,6 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: ``,
-  secret: `mysecret123`
+  endpoint: `https://eu1.prisma.sh/bao-pham-c824d6/test-1/GraphQL_Prisma_Test1`
 });
 exports.prisma = new exports.Prisma();
-var models = [
-  {
-    name: "Link",
-    embedded: false
-  },
-  {
-    name: "User",
-    embedded: false
-  },
-  {
-    name: "Vote",
-    embedded: false
-  }
-];
